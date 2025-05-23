ea49f275188b6af11e5353f6de06eec3c7446b65
prathyushnandan <110914121+prathyushnandan@users.noreply.github.com>
2025-05-23 17:24:32 +0530

    Update Box.java

--- Box.java
+++ Box.java
@@ -2,11 +2,12 @@ public class Box {
     double l = -1;
     double w = -1;
     double h = -1;
-}
+
  public static void main(string[] args) {
      Box b = new Box();
      System.out.println("length:"+b.l);
      System.out.println("width: "+b.w);
      System.out.println("height: "+b.h);
  }
+}
 
